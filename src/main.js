7a9524a26807a98027bd8db583d2d26f60ce389d
大师行 <evan199101@gmail.com>
2019-05-08 23:28:44 +0800

    以class封装cosnole

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import {Log,LogStyle} from './utils/console'
 Vue.config.productionTip = false
+Vue.prototype.$log=Log
+Vue.prototype.$logStyle=LogStyle
 
 /* eslint-disable no-new */
 new Vue({
